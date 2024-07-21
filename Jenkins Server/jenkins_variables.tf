8d978710cafa896aa35a00297d914ba83fad84fe
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-07-21 09:08:26 -0500

    Update jenkins_variables.tf

--- Jenkins Server/jenkins_variables.tf
+++ Jenkins Server/jenkins_variables.tf
@@ -1,10 +1,25 @@
+// Define the name of the SSH key pair used to access the EC2 instance
 variable "mykey" {}
+
+// Define the Amazon Machine Image (AMI) ID for Amazon Linux 2023
 variable "ami" {
   description = "amazon linux 2023 ami"
 }
+
+// Define the AWS region where the resources will be deployed
 variable "region" {}
+
+// Define the type of EC2 instance to launch (e.g., t2.micro)
 variable "instance_type" {}
+
+// Define the name for the security group associated with the Jenkins server
 variable "jenkins_server_secgr" {}
+
+// Define the tag for naming the Jenkins server instance
 variable "jenkins-server-tag" {}
+
+// Define the name for the IAM instance profile for the Jenkins server
 variable "jenkins-profile" {}
+
+// Define the name for the IAM role assigned to the Jenkins server
 variable "jenkins-role" {}
